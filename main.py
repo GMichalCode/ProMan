06cfdaedaa9a05a029523f84119b3a336e42d7de
Damian Trybuszek <damian.trybuszek@gmail.com>
2021-07-07 18:51:17 +0200

    New branches added

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def get_cards_for_board(board_id: int):
     All cards that belongs to a board
     :param board_id: id of the parent board
     """
+    "something to commit in case of adding new branches"
     return queires.get_cards_for_board(board_id)
 
 
