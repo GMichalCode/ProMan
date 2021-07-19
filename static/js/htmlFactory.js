73e412a95287d3bd1b2222638d144e729191d92a
Damian Trybuszek <damian.trybuszek@gmail.com>
2021-07-20 00:48:10 +0200

    Rename boards, columns, board toggle fixed.

--- static/js/htmlFactory.js
+++ static/js/htmlFactory.js
@@ -44,7 +44,7 @@ function cardBuilder(card) {
 
 function columnBuilder(boardId, column) {
     return `<div class="board-column" id="column${column.id}">
-                    <input class="column-title" id=${column.id} value="${column.title}">
+                    <input class="column-title" id="column-title-${column.id}" value="${column.title}">
                     <div class="board${boardId}-column-content" data-column-id="${column.id}"></div>
             </div>`
 }
